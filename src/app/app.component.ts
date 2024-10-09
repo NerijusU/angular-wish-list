a5bc937350f44b241fe687acca6c051c519422ad
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2024-10-09 17:11:43 +0200

    feat: add first view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items = [
+    new WishItem('To learn Angular'),
+    new WishItem('Get Coffe', true),
+    new WishItem('Find Grass'),
+  ];
   title = 'wishlist';
 }
