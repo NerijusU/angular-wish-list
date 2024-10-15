f916fafb6acc5971496bf2a3bab67be9efd5c02b
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2024-10-15 15:26:15 +0200

    feat: add wish-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WishItem } from '../shared/models/wishItem';
+import { WishListComponent } from './wish-list/wish-list.component';
+
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, WishListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -18,23 +25,18 @@ export class AppComponent {
     new WishItem('Find Grass'),
   ];
 
-  listFilter: string = '0';
+  listFilter: any = '0';
 
   newWishText = '';
 
   title = 'wishlist';
 
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
+
   addNewWish() {
     this.items.push(new WishItem(this.newWishText)); // add new wish to items array
     this.newWishText = ''; // clear textbox
-  }
-
-  filterChanged(value: any) {
-    console.log(value);
-  }
-
-  toggleItem(item: WishItem) {
-    item.isComplete = !item.isComplete;
-    console.log(item);
   }
 }
